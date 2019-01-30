b2fe03c1e5ab1e73a1264c8896efcdaa005986cb
Fernando Cardoso <l4nner@users.noreply.github.com>
2019-01-30 10:06:50 -0800

    Replacing the whole file
    Now watching for changes in the number of watched/reported issue. If it changes, it will abandon. You might change this in the future.  Not to abandon. Not a request so far.

--- aeoi.py
+++ aeoi.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-# Beta 2.0
+# Beta 3.0
 
 import os
 import sys
@@ -36,6 +36,7 @@ def alertSound(audioFile,external,internal):
                 subprocess.call("afplay " + audioFile, shell=True)
             time.sleep(0.5)
 
+# If email/login still not available, asks for input
 if not os.path.exists(homeFolder + "/" + configFile):
     userName = raw_input("Enter your Oracle SSO account (Full.Name@Oracle.com): ")
     if re.match(r"\w+\.\w+@oracle.com", userName):
@@ -58,27 +59,38 @@ if (len(sys.argv) == 1) or (len(sys.argv) == 2 and sys.argv[1] in validParameter
     else:
         if sys.argv[1] in ["reporter", "r"]: jiraField="reporter"
         elif sys.argv[1] in ["watcher", "w"]: jiraField="watcher"
-    listInitialComments=[]
+    listCommentsWatermark=[]
     try:
         for issue in jira.search_issues( jiraField +' = currentUser() AND status not in (Resolved, Closed) order by created desc'):
-            listInitialComments.append(len(jira.comments(issue)))
+            listCommentsWatermark.append(len(jira.comments(issue)))
     except BaseException:
         print "Parameter invalid"
         sys.exit()
     try:
+
+        index=0
+        listIssue=[]
+        listSummary=[]
+        listComments=[]
+        
+        # creates ticket list
+        for issue in jira.search_issues(jiraField + ' = currentUser() AND status not in (Resolved, Closed) order by created desc'):
+            listIssue.append(str(issue.key))
+            listSummary.append(str(issue.fields.summary))
+            listComments.append(len(jira.comments(issue)))
+            index += 1
+        
+        # initial number of issues
+        numberIssuesWatermark = (len(listIssue))
+        
+        # enters the loop until number of tickets change or user interrupts
         while True:
             os.system('clear')
             print "\nLast check: ",datetime.datetime.now().strftime("%-I:%M"),"\t*",(jiraField.upper()),"*\n"
             index=0
-            listIssue=[]
-            listSummary=[]
-            listComments=[]
-            for issue in jira.search_issues(jiraField + ' = currentUser() AND status not in (Resolved, Closed) order by created desc'):
-                listIssue.append(str(issue.key))
-                listSummary.append(str(issue.fields.summary))
-                listComments.append(len(jira.comments(issue)))
-                if listComments[index] > listInitialComments[index]:
-                    print "\033[46m",listIssue[index],"\t", listSummary[index],"\033[0m"
+            while index < numberIssuesWatermark:
+                if listComments[index] > listCommentsWatermark[index]:
+                    print "\033[44;96m",listIssue[index],"\t", listSummary[index],"\033[0m"
                     linkPrint(pro, jiraSrvFqdn, str(issue.key))
                     if listAware[index] == False:
                         alertSound("/System/Library/Sounds/Glass.aiff", 2, 2)
@@ -88,6 +100,25 @@ if (len(sys.argv) == 1) or (len(sys.argv) == 2 and sys.argv[1] in validParameter
                 index += 1
             print "\n^C to cancel"
             sleep(120)
+            index=0
+            listIssue=[]
+            listSummary=[]
+            listComments=[]
+            for issue in jira.search_issues(jiraField + ' = currentUser() AND status not in (Resolved, Closed) order by created desc'):
+                listIssue.append(str(issue.key))
+                listSummary.append(str(issue.fields.summary))
+                listComments.append(len(jira.comments(issue)))
+                index += 1
+            if len(listIssue) != numberIssuesWatermark:
+                # if the number of tickets changed, just list all of them and leave
+                print "\n\n* * Number of active items changed to:\n"
+                index=0
+                alertSound("/System/Library/Sounds/Glass.aiff", 2, 2)
+                while index < len(listIssue):
+                    print "\033[90m",listIssue[index],"\t",listSummary[index],"\033[0m"
+                    index += 1
+                print "\n\n"
+                break             
     except KeyboardInterrupt:
         print ""
         pass
@@ -104,14 +135,9 @@ else:
 			    sleep(60)
             else:
                 alertSound("/System/Library/Sounds/Morse.aiff", 1, 3)
-                print "\033[34m* * TICKET UPDATED * *\033[0m\n\t\t"
+                print "\033[44;96* * TICKET UPDATED * *\033[0m\n\t\t"
                 print "\t\t"
                 linkPrint(pro , jiraSrvFqdn , jiraIssue)
                 break
     except BaseException:
         print "Invalid issue number"
-
-
-    
-
-
